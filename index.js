856335016c7446e47b74ff74e435b5636e164f4b
Anna Kravchenko <flamedance.ann@gmail.com>
2022-05-26 15:14:59 +0300

    init: add base markup

---
+++ index.js
@@ -0,0 +1,22 @@
+const body = document.getElementsByTagName("body");
+const darkMode = document.querySelector(".darkmode");
+const options = document.querySelector('.options');
+const optBtns = document.querySelectorAll('.option');
+
+options.addEventListener("click", (event) => {
+  if (event.target.classList.contains("option")) {
+    optBtns.forEach(item => {
+      (item === event.target)
+        ? item.classList.add('active')
+        : item.classList.remove('active');
+    });
+  }
+});
+/*
+darkMode.addEventListener('click', () => {
+	darkMode.toggle("dark").css(darkMode.hasClass("dark")
+				? { background: "#202225", color: "#f9f9f9" }
+				: { background: "#f9f9f9", color: "#202225" }
+		);
+});
+*/
